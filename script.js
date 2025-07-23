99705c7e2f0894c253a0ba53286d0cb668b6a798
Kobi <knjoku@hawk.iit.edu>
2025-07-23 01:55:06 -0500

    Complete shows section

--- script.js
+++ script.js
@@ -7,44 +7,60 @@ async function search(event) {
     `https://api.tvmaze.com/search/shows?q=${showName}`
   );
   const result = await response.json();
+  const searchResults = document.querySelector(".shows");
   const shows = result
-    .map((show) => {
-      `<div class="show">
+    .map((showObject) => {
+      if (
+        showObject.show.name &&
+        showObject.show.language &&
+        showObject.show.summary &&
+        showObject.show.genres[0] &&
+        showObject.show.averageRuntime &&
+        showObject.show.rating.average
+      ) {
+        return `<div class="show">
 <div class="show__description">
-  <h2 class="show__name">${show.name}</h2>
-  <p>Language: <span class="show__description--text">${show.language}</span></p>
-  <p>Genre(s): <span class="show__description--text">${show.genres.join(
-    ","
-  )}</span></p>
-  <p>
+  <h2 class="show__name">${showObject.show.name}</h2>
+  <p>Language: <span class="show__description--text">${
+    showObject.show.language
+  }</span></p>
+    ${
+      showObject.show.genres[0]
+        ? `<p>Genre(s): <span class="show__description--text">${showObject.show.genres.join(
+            ", "
+          )}</span></p>`
+        : ``
+    }
+    ${
+      showObject.show.averageRuntime
+        ? `<p>
     Average Episode Length:
-    <span class="show__description--text">${show.averageRuntime}</span>
+    <span           class="show__description--text">${showObject.show.averageRuntime} minutes</span>
+</p>`
+        : ``
+    }
+  ${
+    showObject.show.rating.average
+      ? `<p>
+  Rating: <span class="show__description--text">${showObject.show.rating.average}/10</span>`
+      : ``
+  }
   </p>
-  <p>
-    Average Rating:
-    <span class="show__description--text">${show.rating.average}</span>
-  </p>
-  <p>
-    Summary:<br /><span class="show__description--text">${extractSummary(
-      show.summary
-    )}</span>
-  </p>
-  <p>Link: <a href="#" class="show__description--text">${
-    show.officialSite
-  }</a></p>
-</div>
+  </div>
 <div class="show__img--wrapper">
   <img
-    src="${show.image.medium}"
-    alt="${show.name}'s poster image"
+    src="${showObject.show.image.medium}"
+    alt="${showObject.show.name}'s Poster Image"
     class="show__img"
   />
+  <a href="#" class="show__img--link">View Summary</a>
 </div>
 </div>`;
+      }
     })
     .join("\n");
-
-  console.log(shows[0]);
+  console.log(shows);
+  searchResults.innerHTML = shows;
 }
 
 function activateSearch() {
@@ -54,7 +70,7 @@ function activateSearch() {
 function extractSummary(summaryFromAPI) {
   const summary = document.createElement("div");
   summary.innerHTML = summaryFromAPI;
-  return summary.textContent.trim();
+  // return summary.textContent.trim();
 }
 
 // show:
@@ -71,3 +87,18 @@ function extractSummary(summaryFromAPI) {
 // summary: "<p>The<b> Regular Show</b> is about Mordecai and Rigby who are two best friends who take the problems and mundane tasks of their boring job to fantastical places.</p>"
 // type: "Animation"
 // image: {medium: 'https://static.tvmaze.com/uploads/images/medium_portrait/6/15947.jpg', original: 'https://static.tvmaze.com/uploads/images/original_untouched/6/15947.jpg'}
+
+
+  // <p>
+  //   Summary:
+  //   <br />
+  //   <span class="show__description--text">
+  //     ${extractSummary(showObject.show.summary)}
+  //   </span>
+  // </p>;
+
+  //   ${
+  //   showObject.show.officialSite
+  //     ? `<p class="show__description--text">Click <b><a href="${showObject.show.officialSite}" target="_blank">here</a></b> for more info.</p>`
+  //     : ``
+  // }

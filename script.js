9b2ef6e122133bd52ef46eecb48c42af09a57d37
Kobi <knjoku@hawk.iit.edu>
2025-07-24 23:53:06 -0500

    Add loading stage and responsiveness.

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const input = document.querySelector(".header__input--bar");
+let yIndex = 0;
 
 async function search(event) {
-  const showName = input.value;
   event.preventDefault();
+  const showName = input.value;
+  const searchResults = document.querySelector(".shows");
+  searchResults.innerHTML = `<i class="fa-solid fa-spinner"></i>`;
+  searchResults.classList.add("loading");
   const response = await fetch(
     `https://api.tvmaze.com/search/shows?q=${showName}`
   );
   const result = await response.json();
-  const searchResults = document.querySelector(".shows");
   const shows = result
     .map((showObject) => {
       if (
@@ -19,60 +22,64 @@ async function search(event) {
         showObject.show.image.medium
       ) {
         return `
-<div class="show--wrapper">
+        <div class="show--wrapper">
         <div class="show">
-<div class="show__description">
-  <h2 class="show__name">${showObject.show.name}</h2>
-  <p>Language: <span class="show__description--text">${
-    showObject.show.language
-  }</span></p>
-<p>Genre(s): <span class="show__description--text">${showObject.show.genres.join(
-          ", "
-        )}</span></p>
-<p>
-    Duration:
-    <span           class="show__description--text">${
-      showObject.show.averageRuntime
-    } minutes</span>
-</p>
-<p>
-Rating: <span class="show__description--text">
-${
-  showObject.show.rating.average
-    ? `${showObject.show.rating.average}/10`
-    : `N/A`
-}
-</span>
+        <div class="show__description">
+        <h2 class="show__name">${showObject.show.name}</h2>
+        <div class="show__details">
+        <p>Language: <span class="show__description--text">${
+          showObject.show.language
+        }</span></p>
+  <p>Genre(s): <span class="show__description--text">${showObject.show.genres.join(
+    ", "
+  )}</span></p>
+  <p>
+  Duration:
+  <span           class="show__description--text">${
+    showObject.show.averageRuntime
+  } minutes</span>
+  </p>
+  <p>
+  Rating: <span class="show__description--text">
+  ${
+    showObject.show.rating.average
+      ? `${showObject.show.rating.average}/10`
+      : `N/A`
+  }
+  </span>
   </p>
   </div>
-<div class="show__img--wrapper">
+  </div>
+  <div class="show__img--wrapper">
   <img
-    src="${showObject.show.image.medium}"
-    alt="${showObject.show.name}'s Poster Image"
-    class="show__img"
+  src="${showObject.show.image.medium}"
+  alt="${showObject.show.name}'s Poster Image"
+  class="show__img"
   />
   <a href="" class="show__img--link" onclick="toggleSummary(event)">View Summary</a>
-</div>
-</div>
-<div class="show__summary-modal--wrapper">
-<div class="show__summary-modal">
+  </div>
+  </div>
+  <div class="show__summary-modal--wrapper">
+  <div class="show__summary-modal">
   <p class="show__summary">${extractSummary(showObject.show.summary)}</p>
-${
-  showObject.show.officialSite
-    ? `<p>
+  ${
+    showObject.show.officialSite
+      ? `<p class="show__link">
     Click <b><a href="${showObject.show.officialSite}" target="_blank">here</a></b> for more information.
-  </p>`
-    : `<p><i>*Link is currently unavailable.</i></p>`
-}
+    </p>`
+      : `<p><i>*Link is currently unavailable.</i></p>`
+  }
   <i class="fa-solid fa-times" onclick="toggleSummary(event)"></i>
-</div>
-</div>
-</div>`;
+  </div>
+  </div>
+  </div>`;
       }
     })
     .join("\n");
-  console.log(result);
-  searchResults.innerHTML = shows;
+  setTimeout(() => {
+    searchResults.classList.remove("loading");
+    searchResults.innerHTML = shows;
+  }, 500);
 }
 
 function activateSearch() {
@@ -86,10 +93,17 @@ function extractSummary(summaryFromAPI) {
 }
 
 function toggleSummary(event) {
-event.preventDefault()
-const selectedShow = event.target.closest(".show--wrapper")
+  event.preventDefault();
+  const selectedShow = event.target.closest(".show--wrapper");
+  if (!document.body.classList.contains("dim")) {
+    yIndex = window.scrollY;
+    document.body.style.top = `-${yIndex}px`;
+    document.body.classList.add("dim");
+  } else {
+    document.body.classList.remove("dim");
+    window.scrollTo(0, yIndex);
+  }
   selectedShow.classList.toggle("view-summary");
-  document.body.classList.toggle("dim")
 }
 
 // show:
